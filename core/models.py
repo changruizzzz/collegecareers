fc550ed5643b38edd33bc67ce5b967c465bb2e7f
Changrui Zhou <changruizzzz@gmail.com>
2019-10-06 17:46:35 -0400

    adding new grads data

--- core/models.py
+++ core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Job(models.Model):
+    TYPE_CHOICE = (
+        ('N', 'New Grads'),
+        ('I', 'Internship')
+    )
+
+    company = models.CharField(max_length=50)
+    type = models.CharField(max_length=1, choices=TYPE_CHOICE, null=False, default='N')
+    title = models.CharField(max_length=200)
+    link = models.URLField()
+    location = models.CharField(max_length=200, blank=True)
+
+    def __str__(self):
+        return '{} - {}'.format(self.company, self.title)
--- core/views.py
+++ core/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
+from .models import Job
 
 
 def index(request):
-    return render(request, 'index.html', {})
+    jobs = Job.objects.all()
+    return render(request, 'index.html', {'jobs': jobs})
